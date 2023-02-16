1819e269f9f6f579b26afec3344039c412f4a831
vladka-oladkaa <vlad.sushchek@gmail.com>
2023-02-16 22:32:29 +0300

    task morse-decoder js

--- src/index.js
+++ src/index.js
@@ -35,9 +35,61 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '*':' '
 };
 
 function decode(expr) {
+    str1 = ''
+    rez=''
+    arr=[]
+    arr3=[]
+    arr2=[]
+
+    for(i=0; i<expr.length; i=i+10){
+     str = expr.slice(i,i+10)
+     arr3.push(str)
+    }
+
+    arr3.forEach(function (element) {
+     str4=element.replace(/11/g, '-')
+     str4=str4.replace(/10/g, '.')
+     arr.push(str4)
+    })
+
+    arr.forEach(function (item){
+
+     if(item.indexOf('-')>=0 && item.indexOf('.')>=0){
+       if(item.indexOf('-')<item.indexOf('.')){
+        str2 = item.slice(item.indexOf('-'))
+        arr2.push(str2)} else { str2 = item.slice(item.indexOf('.'))
+        arr2.push(str2)}
+     }
+
+     if (item.indexOf('-')>=0 && item.indexOf('.')<0){
+        str2 = item.slice(item.indexOf('-'))
+        arr2.push(str2)
+     }
+
+     if (item.indexOf('.')>=0 && item.indexOf('-')<0){
+        str2 = item.slice(item.indexOf('.'))
+        arr2.push(str2)
+     }
+
+     if(item.indexOf('-')<0 && item.indexOf('.')<0){
+        str2 = item.slice(0,1)
+        arr2.push(str2)
+     }
+    })
+
+    arr2.forEach(function (elem){
+     for (let key in MORSE_TABLE){
+        if (elem===key){
+           str1 = str1+MORSE_TABLE[key]
+        }
+     }
+    })
+
+   return(str1)
     // write your solution here
 }
 
